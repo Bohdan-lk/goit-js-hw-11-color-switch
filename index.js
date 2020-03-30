efd2c26dd36a30bf6584f06933ae8f1745c8891e
Bogdan <bogdan.lysenko.k@gmail.com>
2020-03-31 02:28:40 +0300

    fix-5

--- index.js
+++ index.js
@@ -9,27 +9,35 @@ const colors = [
     '#795548',
 ];
 
+
 const startBtn = document.querySelector(`button[data-action="start"]`);
 const stopBtn = document.querySelector(`button[data-action="stop"]`);
-const body = document.querySelector('body');
-let tidspunkt = null;
+
+
 
 const randomIntegerFromInterval = (min, max) => {
     return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
 
-startBtn.addEventListener("click", () => {
-    tidspunkt = setInterval(() => {
-        const num = randomIntegerFromInterval(0, colors.length);
-        console.log(num);
-        const color = colors[num - 1];
-        body.setAttribute('style', `background-color: ${color}`)
-    }, 1000);
-    startBtn.setAttribute('disabled', 'true')
-});
+const setRandomColor =  {
+    isActive: false,
+    start() {
+        if (this.isActive) {
+            return;
+        }
+        this.isActive = true;
+        this.intervalId = setInterval(() => {
+            const setRandomColor = randomIntegerFromInterval(0, colors.length);
+            document.body.style.backgroundColor = colors[setRandomColor]
+        }, 1000);
 
-stopBtn.addEventListener("click", () => {
-    clearInterval(tidspunkt);
-    startBtn.removeAttribute('disabled')
-});
+    },
+    stop() {
+        clearInterval(this.intervalId)
+        this.isActive = false;
+    }
+};
+
+startBtn.addEventListener('click', setRandomColor.start.bind(setRandomColor));
+stopBtn.addEventListener('click', setRandomColor.stop.bind(setRandomColor));
